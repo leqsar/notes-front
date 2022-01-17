af14e09df607ab6812714b4d199b65c9f4998702
leqsar <arteg132579@gmail.com>
2022-01-17 21:08:34 +0300

    headers

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
       method: "GET",
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        "Access-Control-Allow-Methods": "GET, POST",
       }
     });
     const body = await response.json();
